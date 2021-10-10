791dd8cad2021662467ecfd56147115f93758c3d
hamzabahadar <hamzabahadar2000@gmail.com>
2021-10-10 09:16:58 -0700

    day6 completed

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 // ignore: use_key_in_widget_constructors
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,8 +27,8 @@ class LoginPage extends StatelessWidget {
             height: 20.0,
             // child: Text("lmao"),
           ),
-          const Text(
-            "Welcome",
+          Text(
+            "Welcome $name",
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
@@ -36,6 +44,10 @@ class LoginPage extends StatelessWidget {
                   decoration: const InputDecoration(
                     hintText: "Email",
                   ),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
                 ),
                 TextFormField(
                   obscureText: true,
@@ -46,13 +58,49 @@ class LoginPage extends StatelessWidget {
                 const SizedBox(
                   height: 20.0,
                 ),
-                ElevatedButton(
-                  onPressed: () {
+                // ElevatedButton(
+                //   onPressed: () {
+                //     Navigator.pushNamed(context, "/home");
+                //   },
+                //   child: const Text("Login"),
+                //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                // )
+
+                InkWell(
+                  onTap: () async {
+                    setState(() {
+                      changeButton = true;
+                    });
+                    await Future.delayed(Duration(seconds: 1));
                     Navigator.pushNamed(context, "/home");
                   },
-                  child: const Text("Login"),
-                  style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                )
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    width: changeButton ? 50 : 150,
+                    height: 50,
+                    // color: Colors.indigo,
+                    alignment: Alignment.center,
+                    child: changeButton
+                        ? Icon(
+                            Icons.done,
+                            color: Colors.white,
+                          )
+                        : Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                    decoration: BoxDecoration(
+                        color: Colors.indigo,
+                        // shape: changeButton
+                        //     ? BoxShape.circle
+                        //     : BoxShape.rectangle,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8)),
+                  ),
+                ),
               ],
             ),
           ),
